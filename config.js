b427cdbc81188c22b84f4477562dd51d63ee2d5e
nnanyereugodavid97-boop <nnanyereugodavid97@gmail.com>
2026-08-13 17:57:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     
     // Bot Configuration
     botName: 'Nice_Beast👹',
-    prefix: '.',
+    prefix: '~',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
